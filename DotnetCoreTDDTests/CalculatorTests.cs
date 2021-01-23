9510c35db76a7f4e0bfde3f1e46be7dc4f65d98c
handerMacWindows <myginnygo@gmail.com>
2021-01-23 16:36:29 +0800

    第一個測試程式 AddTest

---
+++ DotnetCoreTDDTests/CalculatorTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using DotnetCoreTDD;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotnetCoreTDD.Tests
+{
+    [TestFixture()]
+    public class CalculatorTests
+    {
+        [Test()]
+        public void AddTest()
+        {
+            // Arrange
+            var calculator = new Calculator();
+            int x = 1, y = 2;
+            var expected = 3;
+
+            // Act
+            var actual = calculator.Add(x, y);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

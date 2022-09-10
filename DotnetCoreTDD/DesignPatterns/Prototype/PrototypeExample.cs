75b9b824e7ded256971bfd240fc0c7903d70ceb2
涵德 <hander@linkchain.tw>
2022-09-10 18:13:06 +0800

    feat:(DesignPatterns)新增Prototype模式

---
+++ DotnetCoreTDD/DesignPatterns/Prototype/PrototypeExample.cs
@@ -0,0 +1,76 @@
+﻿using System.Threading;
+
+namespace DotnetCoreTDD.DesignPatterns.Prototype
+{
+    public class PrototypeExample
+    {
+        /// <summary>
+        /// 敵機 Prototype
+        /// </summary>
+        public abstract class EnemyPlanePrototype
+        {
+            /// <summary>敵機X座標</summary>
+            protected int X { get; set; }
+
+            /// <summary>敵機Y座標</summary>
+            protected int Y = 0;
+
+            protected EnemyPlanePrototype(int x)
+            {
+                this.X = x;
+            }
+
+            public int GetX() => X;
+            public int GetY() => Y;
+
+            public abstract EnemyPlanePrototype Clone();
+
+            public void Fly()
+            {
+                this.Y = this.Y + 1;
+            }
+        }
+
+        /// <summary>
+        /// 敵機類別
+        /// </summary>
+        public class EnemyPlane : EnemyPlanePrototype
+        {
+            public EnemyPlane(int x) : base(x)
+            {
+                Thread.Sleep(1); // 睡1毫秒，模擬建立此物件需要一些時間
+            }
+            
+            /// <summary>
+            /// 此方法讓複製後的敵機原型的X座標可以被改變
+            /// </summary>
+            /// <param name="x"></param>
+            public void SetX(int x)
+            {
+                this.X = x;
+            }
+
+            public override EnemyPlanePrototype Clone()
+            {
+                return (EnemyPlanePrototype) this.MemberwiseClone();
+            }
+        }
+
+        /// <summary>
+        /// 敵機工廠
+        /// </summary>
+        public static class EnemyPlaneFactory
+        {
+            /// <summary>
+            /// 用單例餓漢模式(eager initialization，詳見singleton模式範例)造一個敵機原型
+            /// </summary>
+            private static EnemyPlane protoType = new EnemyPlane(200);
+            public static EnemyPlane GetInstance(int x)
+            {
+                var clonePlane = (EnemyPlane) protoType.Clone();
+                clonePlane.SetX(x);
+                return clonePlane;
+            }
+        }
+    }
+}
---
+++ DotnetCoreTDDTests/DesignPatterns/Prototype/PrototypeTests.cs
@@ -0,0 +1,54 @@
+﻿using DotnetCoreTDD.DesignPatterns.Prototype;
+using NUnit.Framework;
+
+namespace DotnetCoreTDDTests.DesignPatterns.Prototype
+{
+    [TestFixture]
+    public class PrototypeTests
+    {
+        [TestCase(100)]
+        [TestCase(200)]
+        [TestCase(300)]
+        public void EnemyPlaneFactory_Returns_Correctly(int x)
+        {
+            // when 取得新敵機
+            var enemyPlane = PrototypeExample.EnemyPlaneFactory.GetInstance(x);
+            
+            // then assert
+            Assert.AreEqual(x, enemyPlane.GetX());
+        }
+
+        
+        /// <summary>
+        /// 測試利用 prototype 建立物件的效能比較好
+        /// </summary>
+        [Test]
+        public void CreateInstance_WithPrototype_IsMoreEfficient()
+        {
+            // given 重覆次數
+            int loopTime = 100;
+            
+            // when 以一般方式建立instance
+            var beforeDT = System.DateTime.Now;
+            for (int i = 0; i < loopTime; i++)
+            {
+                new PrototypeExample.EnemyPlane(i);
+            }
+            var afterDT = System.DateTime.Now;
+            var tsForGeneral = afterDT.Subtract(beforeDT).TotalMilliseconds;
+            
+            // when 以prototype方式建立instance
+            beforeDT = System.DateTime.Now;
+            for (int i = 0; i < loopTime; i++)
+            {
+                PrototypeExample.EnemyPlaneFactory.GetInstance(i);
+            }
+            afterDT = System.DateTime.Now;
+            var tsForPrototype = afterDT.Subtract(beforeDT).TotalMilliseconds;
+            
+            
+            // then assert prototype 方法建立的時間比較短
+            Assert.Less(tsForPrototype, tsForGeneral);
+        }
+    }
+}

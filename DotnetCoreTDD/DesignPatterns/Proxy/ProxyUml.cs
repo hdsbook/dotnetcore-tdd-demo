36d1d267990bb48f1c287fa4379fc71df8d14820
涵德 <hander@linkchain.tw>
2022-11-16 21:48:21 +0800

    feat:(DesignPatterns)新增Proxy Uml範例

---
+++ DotnetCoreTDD/DesignPatterns/Proxy/ProxyUml.cs
@@ -0,0 +1,49 @@
+﻿namespace DotnetCoreTDD.DesignPatterns.Proxy
+{
+    public class ProxyUml
+    {
+        public interface IService
+        {
+            public string Operation();
+        }
+        
+        public class RealService: IService
+        {
+            public string Operation()
+            {
+                return "ok";
+            }
+        }
+        
+        public class ProxyAlwaysDeny: IService
+        {
+            public IService Service { get; set; }
+
+            public ProxyAlwaysDeny(IService service)
+            {
+                Service = service;
+            }
+
+
+            public string Operation()
+            {
+                var result = string.Empty;
+                
+                // 做權限驗證，驗證通過才執行
+                if (CheckAccess())
+                {
+                    result = Service.Operation();
+
+                    // maybe 這邊還可以做一些 log 紀錄
+                }
+
+                return result;
+            }
+
+            public virtual bool CheckAccess()
+            {
+                return false; // 這邊採取總是拒絕
+            }
+        }
+    }
+}
---
+++ DotnetCoreTDDTests/DesignPatterns/Proxy/ProxyTests.cs
@@ -0,0 +1,37 @@
+﻿using DotnetCoreTDD.DesignPatterns.Proxy;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DotnetCoreTDDTests.DesignPatterns.Proxy
+{
+    [TestFixture]
+    public class ProxyTests
+    {
+        [Test]
+        public void Operation_WithoutProxy_WorksCorrectly()
+        {
+            // given service
+            var service = new ProxyUml.RealService();
+
+            // when do operation
+            var result = service.Operation();
+            
+            // then
+            result.Should().Be("ok");
+        }
+        
+        [Test]
+        public void Operation_WithDenyProxy_WorksCorrectly()
+        {
+            // given service
+            ProxyUml.IService service = new ProxyUml.RealService();
+
+            // when wrap service with proxy and do operation
+            service = new ProxyUml.ProxyAlwaysDeny(service);
+            var result = service.Operation();
+            
+            // then
+            result.Should().BeEmpty();
+        }
+    }
+}

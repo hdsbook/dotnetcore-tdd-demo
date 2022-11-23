0a19647bbfe59511dc59c7e60a5f1b9d597caa9e
涵德 <hander@linkchain.tw>
2022-11-23 16:57:57 +0800

    feat:(DesignPatterns)新增Mediator模式Uml範例

---
+++ DotnetCoreTDD/DesignPatterns/Mediator/MediatorUml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotnetCoreTDD.DesignPatterns.Mediator
+{
+    public class MediatorUml
+    {
+        public interface IMediator
+        {
+            void Register(IComponent component);
+        }
+        
+        public interface IComponent
+        {
+            void SetMediator(ConcreteMediator mediator);
+            void ReceiveMessage(string message);
+        }
+
+        public class ConcreteMediator : IMediator
+        {
+            private readonly List<IComponent> Components = new List<IComponent>();
+            public void Register(IComponent component)
+            {
+                Components.Add(component);
+                component.SetMediator(this);
+            }
+
+            public void DistributeMessage(string message)
+            {
+                foreach (var component in Components)
+                {
+                    component.ReceiveMessage(message);
+                }
+            }
+        }
+
+
+        public class ConcreteComponentA : IComponent
+        {
+            private readonly List<string> ReceivedMessages = new List<string>();
+            private ConcreteMediator _mediator = null;
+
+            public void SetMediator(ConcreteMediator mediator)
+            {
+                _mediator = mediator;
+            }
+            
+            public void ReceiveMessage(string message)
+            {
+                ReceivedMessages.Add(message);
+            }
+
+            public void SendMessage(string message)
+            {
+                _mediator.DistributeMessage(message);
+            }
+
+            public List<string> GetReceivedMessages()
+            {
+                return ReceivedMessages;
+            }
+        }
+        
+        public class ConcreteComponentB : IComponent
+        {
+            private readonly List<string> ReceivedMessages = new List<string>();
+            private ConcreteMediator _mediator = null;
+
+            public void SetMediator(ConcreteMediator mediator)
+            {
+                _mediator = mediator;
+            }
+            
+            public void ReceiveMessage(string message)
+            {
+                // 假設接受訊息時，會多加紀錄日期資訊
+                message = DateTime.Now.ToString("yyyy/MM/dd") + ":" + message;
+                ReceivedMessages.Add(message);
+            }
+
+            public void SendMessage(string message)
+            {
+                // 可能會在發送訊息時為訊息加上一個前綴
+                _mediator.DistributeMessage("[From B]" + message);
+            }
+
+            public List<string> GetReceivedMessages()
+            {
+                return ReceivedMessages;
+            }
+        }
+    }
+
+}
---
+++ DotnetCoreTDDTests/DesignPatterns/Mediator/MediatorTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using DotnetCoreTDD.DesignPatterns.Decorator;
+using DotnetCoreTDD.DesignPatterns.Mediator;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DotnetCoreTDDTests.DesignPatterns.Mediator
+{
+    [TestFixture]
+    public class MediatorTests
+    {
+        [Test]
+        public void GivenMediator_WhenRegisterComponents_WorksCorrectly()
+        {
+            var today = DateTime.Now.ToString("yyyy/MM/dd");
+
+            // given mediator and components
+            var mediator = new MediatorUml.ConcreteMediator();
+            var componentA = new MediatorUml.ConcreteComponentA();
+            var componentB = new MediatorUml.ConcreteComponentB();
+
+            // when register components
+            mediator.Register(componentA);
+            mediator.Register(componentB);
+
+            // when components send messages
+            componentA.SendMessage("來自A的訊息");
+            componentB.SendMessage("來自B的訊息");
+
+            // then 驗證各components 收到的訊息正確
+            var receivedMessagesOfA = componentA.GetReceivedMessages();
+            receivedMessagesOfA.Should().BeEquivalentTo(new List<string>
+            {
+                "來自A的訊息",
+                "[From B]來自B的訊息"
+            });
+
+            var receivedMessagesOfB = componentB.GetReceivedMessages();
+            receivedMessagesOfB.Should().BeEquivalentTo(new List<string>
+            {
+                today + ":" + "來自A的訊息",
+                today + ":" + "[From B]來自B的訊息"
+            });
+        }
+    }
+}
